d559a8970a93665f66a0f75634f97d1ee0cd33f2
Devin Xie <devinxie418@gmail.com>
2026-07-28 22:54:24 -0400

    feat: appLayout

--- src/components/screens/AppLayout/AppLayout.tsx
+++ src/components/screens/AppLayout/AppLayout.tsx
@@ -1,10 +1,22 @@
+import { Outlet } from "react-router"
 
+// components
+import { MobileLeftSidebar } from "./MobileSidebar/MobileLeftSidebar"
+import { MobileRightSidebar } from "./MobileSidebar/MobileRightSidebar"
+import { LeftSidebar } from "./Sidebar/LeftSidebar"
+import { RightSidebar } from "./Sidebar/RightSidebar"
 
 export function AppLayout() {
 
     return (
-        <>
-        
-        </>
+        <div
+        className="w-full h-screen flex"
+        >
+            <LeftSidebar />
+
+            <Outlet />
+
+            <RightSidebar />
+        </div>
     )
 }
---
+++ src/components/screens/AppLayout/MobileSidebar/MobileLeftSidebar.tsx
@@ -0,0 +1,8 @@
+
+
+export function MobileLeftSidebar() {
+    return (
+        <>
+        </>
+    )
+}
